ed821cb15bc113ccbe5b571d061c90dd6e1b2ede
tubone <tubo.yyyuuu@gmail.com>
2024-02-24 17:39:49 +0900

    init

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -5,8 +5,7 @@ describe("UI Test", () => {
     today.setMonth(today.getMonth() + 1);
     const year = today.getFullYear();
     const month = today.getMonth() + 1; // getMonth()は0から始まるため、実際の月の数に1を加える
-    const formattedMonth = month < 10 ? '0' + month : month; // 1桁の月を2桁の文字列に変換する
-    const result = year + '年' + formattedMonth + '月';
+    const result = year + '年' + month + '月';
     cy.contains(result).click();
     cy.wait(30000);
     cy.screenshot({ clip: {x: 285, y: 45, width: 1512, height: 600}});
