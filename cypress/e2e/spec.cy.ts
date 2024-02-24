8a6acfad9fb2ea9d2b7121c8c630c71d8e3bdacc
tubone(Yu Otsubo) <tubo.yyyuuu@gmail.com>
2024-02-24 18:03:56 +0900

    Update spec.cy.ts

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,4 +1,17 @@
 describe("UI Test", () => {
+  beforeEach(() =>
+  cy.intercept(/.*\.pdf$/, (req) => {
+    const pdfName = req.url.split("/").at(-1);
+    console.log("Downloading pdf instead of opening in browser", {
+      url: req.url,
+      pdfName,
+    });
+
+    req.continue((res) => {
+      res.headers["Content-Disposition"] = `attachment; filename=${pdfName};`;
+    });
+  })
+);
   it("access caldo", () => {
     cy.visit("https://www.hotyoga-caldo.com/mizonokuchi/studio/");
     const today = new Date();
@@ -7,7 +20,7 @@ describe("UI Test", () => {
     const month = today.getMonth() + 1; // getMonth()は0から始まるため、実際の月の数に1を加える
     const result = year + '年' + month + '月';
     cy.contains(result).click();
-    cy.wait(30000);
-    cy.screenshot({ clip: {x: 285, y: 45, width: 1512, height: 600}});
+    // cy.wait(30000);
+    // cy.screenshot({ clip: {x: 285, y: 45, width: 1512, height: 600}})
   })
 });
