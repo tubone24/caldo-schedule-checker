ab21a994e6ddd424985ef44fb926d947d418f144
tubone <tubo.yyyuuu@gmail.com>
2024-02-24 17:30:42 +0900

    init

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,9 @@
 describe("UI Test", () => {
   it("access caldo", () => {
     cy.visit("https://www.hotyoga-caldo.com/mizonokuchi/studio/");
+    const date = new Date();
     cy.contains("2024年3月").click();
-    cy.wait(10000);
-    cy.wait(1000)
+    cy.wait(30000);
     cy.screenshot({ clip: {x: 285, y: 45, width: 1512, height: 600}});
   })
 });
